94e6ab06d272534ea90f3f4a9faf74a6c38a9182
ykhells <97477387+ykhells@users.noreply.github.com>
2022-01-10 15:24:01 -0500

    Create spammer.py

---
+++ spammer.py
@@ -0,0 +1,14 @@
+#Make sure to pip install pyautogui before running
+
+import pyautogui, time
+
+time.sleep(1)
+file_name = input("Whats the name of the file?: ")
+
+f = open(file_name, 'r')
+for word in f:
+    pyautogui.typewrite(word)
+    pyautogui.press("enter")
+    
+    explode#0001
+    @chromosomes
